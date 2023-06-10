2c832849b02996953c0ae399b563a60dcdd22bc8
fei <2771360979@qq.com>
2023-06-10 17:16:20 +0800

    上传头像

--- src/main/java/com/it/controller/UserController.java
+++ src/main/java/com/it/controller/UserController.java
@@ -4,6 +4,7 @@ import com.it.pojo.Result;
 import com.it.pojo.User;
 import com.it.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,6 +17,37 @@ import java.nio.channels.MulticastChannel;
 public class UserController {
     @Autowired
     private UserService userService;
+    @Value("${fei.path}")
+    private String basePath;
+
+    /**
+     * 上传头像
+     * @param uid
+     * @param file
+     * @return
+     */
+    @RequestMapping("head")
+    public Result head(String uid,MultipartFile file){
+        //获取原始文件名
+        String originalFilename = file.getOriginalFilename();
+        //获取后缀
+        String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+        //以uid重命名
+        String name = uid+suffix;
+        //创建目录
+        File dir = new File(basePath);
+        if (!dir.exists()){
+            //若目录为空则新建目录
+            dir.mkdirs();
+        }
+        //转存
+        try {
+            file.transferTo(new File(basePath+name));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Result.success();
+    }
 
     @RequestMapping("register")
     public Result register(User user){
@@ -32,9 +64,5 @@ public class UserController {
         if(logon ==null) return Result.error("账号密码错误");
         return Result.success(logon);
     }
-    @RequestMapping("head")
-    public Result head(MultipartFile ima,User user) throws IOException {
-        ima.transferTo(new File("D:\\微信开发者工具\\项目\\springboot\\ima"+user.getUid()+".jpg"));
-        return Result.success();
-    }
+
 }
